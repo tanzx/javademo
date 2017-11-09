fa4c828d711ef91d483f090dea3d8d3116e3abfb
tanzx <ungrastutan86@163.com>
2017-11-09 16:52:21 +0800

    Update MyCommand.java

--- src/com/tan/springdemo/lookupmethod/MyCommand.java
+++ src/com/tan/springdemo/lookupmethod/MyCommand.java
@@ -1,4 +1,4 @@
-package com.tan.springdemo.trial.lookupmethod;
+package com.tan.springdemo.lookupmethod;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
