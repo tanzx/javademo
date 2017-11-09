1102f3f81e4c648b5dccb65089c82c4ca4e2ea39
tanzx <ungrastutan86@163.com>
2017-11-09 17:05:39 +0800

    Update CommandManager.java

--- src/com/tan/springdemo/lookupmethod/CommandManager.java
+++ src/com/tan/springdemo/lookupmethod/CommandManager.java
@@ -3,6 +3,10 @@ package com.tan.springdemo.lookupmethod;
 import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.stereotype.Component;
 
+// Spring怎么生成CommandManager的实例呢？抽象类不能通过构造函数创建对象。
+// Spring生成了一个继承CommandManager的代理类，负责实现createCommand方法。
+// createCommand方法实现了定制的Command对象创建功能。
+// 这里实现了往一个Singleton bean中注入Prototype bean的功能。
 @Component
 public abstract class CommandManager {
 	public String process(String commandState) {
