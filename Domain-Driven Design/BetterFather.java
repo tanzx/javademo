516bfd11adb867882c98bb0c432bddac442121c8
tanzx <ungrastutan86@163.com>
2018-01-23 17:09:58 +0800

    Update BetterFather.java

--- Domain-Driven Design/BetterFather.java
+++ Domain-Driven Design/BetterFather.java
@@ -1,5 +1,9 @@
 public class BetterFather{
     //private Son son; 删除这个引用
     private SonRepository sonRepo;// 添加一个 Son 的 repo
+    
     private getSon(){return sonRepo.getByFatherId(this.id);}
+    
+    public BetterFather(SonRepository sonRepo){this.sonRepo = sonRepo;}
+
 }
